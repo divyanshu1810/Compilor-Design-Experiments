215f8447d2f365a02a3d909a8e09d180fa740f00
Divyanshu Yadav <divyanshu.1810@gmail.com>
2024-05-12 23:33:39 +0530

    feat: add three address

---
+++ three_address.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string a = "a";
+    string b = "b";
+    string c = "c";
+    string d = "d";
+    string temp1 = "t1";
+    string temp2 = "t2";
+
+    // Generating three address code
+    cout << temp1 << " = " << c << " * " << d << endl;
+    cout << temp2 << " = " << b << " + " << temp1 << endl;
+    cout << a << " = " << temp2 << endl;
+
+    return 0;
+}
